3cdda9c53ca08f2f150937f793e053f3f5f1729d
Marian Petlovanyi <petlyovanyj.maryan@gmail.com>
2022-11-08 18:58:57 +0200

    hotfix

--- script.sh
+++ script.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-sudo apt update
-sudo apt install apache2
-sudo apt install ufw
-sudo ufw status verbose
-sudo ufw enable
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow 80/tcp
-sudo ufw allow from 127.0.0.1
-sudo ufw status
-sudo mkdir /var/www/marianpetlovanyi/
-sudo touch /var/www/marianpetlovanyi/index.html
-sudo sh -c "echo '<html><html><body><h1>Marian Petlovanyi</h1></body></html>' > /var/www/marianpetlovanyi/index.html"
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/marianpetlovanyi.conf
-sudo sh -c "echo '<VirtualHost *:80>\n\tServerAdmin petlyovanyy@gmail.com \n\tDocumentRoot /var/www/marianpetlovanyi \n\tServerName marianpetlovanyi.example.com \n\tErrorLog ${APACHE_LOG_DIR}/error.log \n\tCustomLog ${APACHE_LOG_DIR}/access.log combined \n</VirtualHost>' > /etc/apache2/sites-available/marianpetlovanyi.conf"
-sudo a2ensite marianpetlovanyi.conf
-sudo sh -c "echo '127.0.0.1 marianpetlovanyi.example.com' >> /etc/hosts"
-sudo service apache2 restart
-sudo systemctl reload apache2
-firefox marianpetlovanyi.example.com
-
-
-
-
-
-
-
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+apt update
+apt install apache2
+apt install ufw
+apt install -y xdg-utils
+ufw status verbose
+ufw enable
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 80/tcp
+ufw allow from 127.0.0.1
+ufw status
+if [ ! -d "/var/www/marianpetlovanyi/" ]; then
+	mkdir /var/www/marianpetlovanyi/
+fi
+touch /var/www/marianpetlovanyi/index.html
+echo '<html><html><body><h1>Marian Petlovanyi</h1></body></html>' > /var/www/marianpetlovanyi/index.html
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/marianpetlovanyi.conf
+sh -c "echo '<VirtualHost *:80>\n\tServerAdmin petlyovanyy@gmail.com \n\tDocumentRoot /var/www/marianpetlovanyi \n\tServerName marianpetlovanyi.example.com \n\tErrorLog ${APACHE_LOG_DIR}/error.log \n\tCustomLog ${APACHE_LOG_DIR}/access.log combined \n</VirtualHost>' > /etc/apache2/sites-available/marianpetlovanyi.conf"
+a2ensite marianpetlovanyi.conf
+sh -c "echo '127.0.0.1 marianpetlovanyi.example.com' >> /etc/hosts"
+service apache2 restart
+systemctl reload apache2
